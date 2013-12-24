f979e80f3455d85b41e08e0ec4bb9020fac99a28
Adan <adan.scotney@gmail.com>
2013-12-24 15:35:56 +0000

    New drawing function and state passing

--- main.scm
+++ main.scm
@@ -10,6 +10,7 @@
 							(srfi srfi-9 gnu)
 							(engine scene)
 							(engine settings)
+              (engine drawing)
 							(game game))
 							
 ; handle quit event
@@ -21,14 +22,14 @@
 	)
 	
 ; event handler
-(define (handle-event scene)
+(define (handle-event scene state)
 	(let ((e (SDL:make-event)))
 		(if (SDL:poll-event e)
 			; event happened
 			(case (SDL:event:type e)
 				((quit) (handle-quit))
-				((key-down) (begin (log! 'DEBUG "Pressed[" (SDL:event:key:keysym:sym e) "]") ((scene-handle-key-down scene) (SDL:event:key:keysym:sym e))))
-				((key-up) (begin (log! 'DEBUG "Released[" (SDL:event:key:keysym:sym e) "]") ((scene-handle-key-up scene) (SDL:event:key:keysym:sym e))))
+				((key-down) (begin (log! 'DEBUG "Pressed[" (SDL:event:key:keysym:sym e) "]") ((scene-handle-key-down scene) (SDL:event:key:keysym:sym e) state)))
+				((key-up) (begin (log! 'DEBUG "Released[" (SDL:event:key:keysym:sym e) "]") ((scene-handle-key-up scene) (SDL:event:key:keysym:sym e) state)))
 				(else do-nothing)
 				)
 			; no event
@@ -42,28 +43,28 @@
   (let* ((current-time (SDL:get-ticks))
          (time-taken (- current-time last-time)))
     (set! last-time current-time)
-    (set! average-time-per-frame (+ (* average-time-per-frame 0.975) (* time-taken 0.025)))
+    (set! average-time-per-frame (+ (* average-time-per-frame 0.8) (* time-taken 0.2)))
   )
   (round (/ 1000 average-time-per-frame)))
 			
 ; main game loop
-(define (main-loop settings scene fps-manager master-resources)
+(define (main-loop settings scene fps-manager master-resources state)
   (SDL:fps-manager-delay! fps-manager) ; perform variable delay based on given FPS
-	(handle-event scene)
+	(handle-event scene state)
 
-;  (let ((screen (SDL:display-format (SDL:make-surface (settings-screen-width settings) (settings-screen-height settings)))))
   (let ((screen (SDL:get-video-surface)))
-	  ((scene-render scene) screen (scene-resources scene))
+    (SDL:fill-rect screen #f (SDL:map-rgb (SDL:surface-get-format screen) BLACK))
+	  ((scene-render scene) screen (scene-resources scene) state)
 		; copy this surface to the screen
 		(SDL:blit-surface screen)
     ; render fps
     (if (settings-show-fps settings)
-      (SDL:blit-surface (SDL:render-text (find-font master-resources "fifteen16") (number->string (recalculate-fps)) (SDL:make-color 255 255 255) (SDL:make-color 0 0 0))))
+      (SDL:blit-surface (SDL:render-text (find-font master-resources "fifteen16") (number->string (recalculate-fps)) WHITE BLACK)))
     ; flip
 		(SDL:flip))
 
   ; loop
-	(main-loop settings scene fps-manager master-resources)
+	(main-loop settings scene fps-manager master-resources state)
 	)
 	
 ; prepare sdl
@@ -84,14 +85,16 @@
   (set! average-time-per-frame (/ 1000 fps))
 
 	; jump into game
-  (let ((master-resources 
+  (let ((initial-game-scene (make-game-initial-scene))
+        (master-resources 
           (make-resources
             '(FONT "fifteen16" "resources/FifteenNarrow.ttf" 16))))
 	  (main-loop 
       (make-settings width height show-fps) 
-      (make-game-initial-scene)
+      (cdr initial-game-scene)
       (SDL:make-fps-manager fps)
-      master-resources)))
+      master-resources
+      (car initial-game-scene))))
 
 ; entry point
 (init 'DEBUG 320 240 16 60 #t)
